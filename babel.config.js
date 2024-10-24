ec10ac04aae989b71b4a463a1940a0aa7e6f1f07
Shiv <shiv.tiwari@validusfintech.com>
2024-10-24 17:51:20 +0530

    feat: flavour is working

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
-// babel.config.js
 module.exports = {
   presets: [
     'module:metro-react-native-babel-preset',
     '@babel/preset-typescript', // If using TypeScript
+  ],
+  plugins: [
+    ['@babel/plugin-transform-class-properties', {loose: true}],
+    ['@babel/plugin-transform-private-methods', {loose: true}],
+    ['@babel/plugin-transform-private-property-in-object', {loose: true}],
   ],
 };

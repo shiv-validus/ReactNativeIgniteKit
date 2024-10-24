b1509851b45714479bce3b80777ea9212792403f
Shiv <shiv.tiwari@validusfintech.com>
2024-10-24 15:26:58 +0530

    fix: UI

--- App.tsx
+++ App.tsx
@@ -6,6 +6,9 @@ import {
   Text,
   StyleSheet,
   View,
+  TouchableOpacity,
+  Alert,
+  Linking
 } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import Config from 'react-native-config';
@@ -19,6 +22,19 @@ function App(): React.JSX.Element {
   // Get the API URL from the environment configuration
   const apiUrl = Config.API_URL;
 
+  // Function to handle button press
+  const handleLearnMorePress = async () => {
+    const url = 'https://www.netflix.com/in/';
+    // Check if the URL can be opened
+    const supported = await Linking.canOpenURL(url);
+    if (supported) {
+      // Open the URL in the default browser
+      await Linking.openURL(url);
+    } else {
+      Alert.alert('Error', 'Unable to open URL');
+    }
+  };
+
   return (
     <SafeAreaView style={[styles.container, backgroundStyle]}>
       <StatusBar
@@ -29,6 +45,10 @@ function App(): React.JSX.Element {
         <Text style={styles.title}>Welcome to Netflix App!</Text>
         <Text style={styles.subtitle}>Current Environment:</Text>
         <Text style={styles.apiUrl}>{apiUrl}</Text>
+        <TouchableOpacity style={styles.button} onPress={() => handleLearnMorePress()}
+        >
+          <Text style={styles.buttonText}>Learn More</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -45,27 +65,50 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   title: {
-    fontSize: 24,
+    fontSize: 28,
     fontWeight: 'bold',
+    color: '#E50914', // Netflix Red
     marginBottom: 8,
     textAlign: 'center',
   },
   subtitle: {
-    fontSize: 18,
+    fontSize: 20,
     color: 'gray',
     marginBottom: 4,
     textAlign: 'center',
   },
   apiUrl: {
     fontSize: 16,
-    color: 'blue',
+    color: '#007AFF', // A more vibrant blue
     fontStyle: 'italic',
     textAlign: 'center',
     borderWidth: 1,
-    borderColor: 'blue',
+    borderColor: '#007AFF',
     padding: 10,
     borderRadius: 5,
     marginTop: 10,
+    backgroundColor: '#E6F7FF', // Light background for the URL
+  },
+  button: {
+    marginTop: 20,
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 5,
+    backgroundColor: '#E50914', // Netflix Red
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 2.62,
+    elevation: 4, // For Android shadow
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 

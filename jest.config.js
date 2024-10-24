61717e4a3ae0ca5affcb30f3ec7a7f02d2603cc0
Shiv <shiv.tiwari@validusfintech.com>
2024-10-24 15:48:57 +0530

    chore: set up mock for react-native-config to ensure smooth test execution

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,6 @@
-// jest.config.js
 module.exports = {
   preset: 'react-native',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest', // Transform TypeScript files
-    '^.+\\.jsx?$': 'babel-jest', // Transform JavaScript files if necessary
+  moduleNameMapper: {
+    'react-native-config': '<rootDir>/__mocks__/react-native-config.js',
   },
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
-  testPathIgnorePatterns: ['/node_modules/', '/build/'], // Ignore node_modules and build
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json', // Use your tsconfig file
-    },
-  },
-  // Optionally add more configurations based on your project needs
 };
